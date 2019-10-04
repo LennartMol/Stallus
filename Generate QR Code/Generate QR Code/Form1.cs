c2f711c163d87fb11144678ed90f5a2b6d9e61a8
Lennart Mol <LennartMol@users.noreply.github.com>
2019-10-04 14:01:10 +0200

    QR Code Generator
    Een C# programma om een QR Code te genereren. Eventueel kan een project logo toegevoegd worden.

---
+++ Generate QR Code/Generate QR Code/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QRCoder;
+
+namespace Generate_QR_Code
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode("The greatest Pepe.", QRCodeGenerator.ECCLevel.H);
+            QRCode qrCode = new QRCode(qrCodeData);     
+            Bitmap customImage = new Bitmap(Generate_QR_Code.Properties.Resources.pepe);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20, Color.Black, Color.White, customImage, 27);
+            pictureBox1.Image = qrCodeImage;
+        }
+    }
+}
