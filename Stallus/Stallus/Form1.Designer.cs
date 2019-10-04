35ffaf5f6900cc672aeda2534f84ff0ea8123833
GlennHoeks <55874566+GlennHoeks@users.noreply.github.com>
2019-10-04 11:58:19 +0200

    Saldo toegevoed UI

--- Stallus/Stallus/Form1.Designer.cs
+++ Stallus/Stallus/Form1.Designer.cs
@@ -33,12 +33,25 @@
             this.lOutCheckTime = new System.Windows.Forms.Label();
             this.btnCheckIn = new System.Windows.Forms.Button();
             this.btnCheckOut = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lSaldo = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rb5 = new System.Windows.Forms.RadioButton();
+            this.rb10 = new System.Windows.Forms.RadioButton();
+            this.rb15 = new System.Windows.Forms.RadioButton();
+            this.rb20 = new System.Windows.Forms.RadioButton();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // cbLocation
             // 
             this.cbLocation.FormattingEnabled = true;
-            this.cbLocation.Location = new System.Drawing.Point(9, 15);
+            this.cbLocation.Location = new System.Drawing.Point(6, 6);
             this.cbLocation.Name = "cbLocation";
             this.cbLocation.Size = new System.Drawing.Size(121, 24);
             this.cbLocation.TabIndex = 0;
@@ -46,7 +59,7 @@
             // lInCheckTime
             // 
             this.lInCheckTime.AutoSize = true;
-            this.lInCheckTime.Location = new System.Drawing.Point(9, 89);
+            this.lInCheckTime.Location = new System.Drawing.Point(6, 80);
             this.lInCheckTime.Name = "lInCheckTime";
             this.lInCheckTime.Size = new System.Drawing.Size(46, 17);
             this.lInCheckTime.TabIndex = 1;
@@ -55,7 +68,7 @@
             // lOutCheckTime
             // 
             this.lOutCheckTime.AutoSize = true;
-            this.lOutCheckTime.Location = new System.Drawing.Point(249, 89);
+            this.lOutCheckTime.Location = new System.Drawing.Point(246, 80);
             this.lOutCheckTime.Name = "lOutCheckTime";
             this.lOutCheckTime.Size = new System.Drawing.Size(46, 17);
             this.lOutCheckTime.TabIndex = 2;
@@ -63,7 +76,7 @@
             // 
             // btnCheckIn
             // 
-            this.btnCheckIn.Location = new System.Drawing.Point(9, 125);
+            this.btnCheckIn.Location = new System.Drawing.Point(6, 116);
             this.btnCheckIn.Name = "btnCheckIn";
             this.btnCheckIn.Size = new System.Drawing.Size(80, 25);
             this.btnCheckIn.TabIndex = 3;
@@ -72,27 +85,138 @@
             // 
             // btnCheckOut
             // 
-            this.btnCheckOut.Location = new System.Drawing.Point(252, 125);
+            this.btnCheckOut.Location = new System.Drawing.Point(249, 116);
             this.btnCheckOut.Name = "btnCheckOut";
             this.btnCheckOut.Size = new System.Drawing.Size(80, 25);
             this.btnCheckOut.TabIndex = 4;
             this.btnCheckOut.Text = "Check out";
             this.btnCheckOut.UseVisualStyleBackColor = true;
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(454, 426);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.cbLocation);
+            this.tabPage1.Controls.Add(this.btnCheckOut);
+            this.tabPage1.Controls.Add(this.lOutCheckTime);
+            this.tabPage1.Controls.Add(this.btnCheckIn);
+            this.tabPage1.Controls.Add(this.lInCheckTime);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(446, 397);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Stand";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.rb20);
+            this.tabPage2.Controls.Add(this.rb15);
+            this.tabPage2.Controls.Add(this.rb10);
+            this.tabPage2.Controls.Add(this.rb5);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.lSaldo);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(446, 397);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Saldo";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Saldo:";
+            // 
+            // lSaldo
+            // 
+            this.lSaldo.AutoSize = true;
+            this.lSaldo.Location = new System.Drawing.Point(58, 45);
+            this.lSaldo.Name = "lSaldo";
+            this.lSaldo.Size = new System.Drawing.Size(16, 17);
+            this.lSaldo.TabIndex = 1;
+            this.lSaldo.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Raise saldo";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // rb5
+            // 
+            this.rb5.AutoSize = true;
+            this.rb5.Location = new System.Drawing.Point(9, 112);
+            this.rb5.Name = "rb5";
+            this.rb5.Size = new System.Drawing.Size(58, 21);
+            this.rb5.TabIndex = 3;
+            this.rb5.Text = "€ 5,-";
+            this.rb5.UseVisualStyleBackColor = true;
+            // 
+            // rb10
+            // 
+            this.rb10.AutoSize = true;
+            this.rb10.Location = new System.Drawing.Point(9, 139);
+            this.rb10.Name = "rb10";
+            this.rb10.Size = new System.Drawing.Size(66, 21);
+            this.rb10.TabIndex = 4;
+            this.rb10.Text = "€ 10,-";
+            this.rb10.UseVisualStyleBackColor = true;
+            // 
+            // rb15
+            // 
+            this.rb15.AutoSize = true;
+            this.rb15.Location = new System.Drawing.Point(9, 166);
+            this.rb15.Name = "rb15";
+            this.rb15.Size = new System.Drawing.Size(66, 21);
+            this.rb15.TabIndex = 5;
+            this.rb15.Text = "€ 15,-";
+            this.rb15.UseVisualStyleBackColor = true;
+            // 
+            // rb20
+            // 
+            this.rb20.AutoSize = true;
+            this.rb20.Checked = true;
+            this.rb20.Location = new System.Drawing.Point(9, 193);
+            this.rb20.Name = "rb20";
+            this.rb20.Size = new System.Drawing.Size(66, 21);
+            this.rb20.TabIndex = 6;
+            this.rb20.TabStop = true;
+            this.rb20.Text = "€ 20,-";
+            this.rb20.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnCheckOut);
-            this.Controls.Add(this.btnCheckIn);
-            this.Controls.Add(this.cbLocation);
-            this.Controls.Add(this.lInCheckTime);
-            this.Controls.Add(this.lOutCheckTime);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -103,6 +227,16 @@
         private System.Windows.Forms.Label lOutCheckTime;
         private System.Windows.Forms.Button btnCheckIn;
         private System.Windows.Forms.Button btnCheckOut;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.RadioButton rb20;
+        private System.Windows.Forms.RadioButton rb15;
+        private System.Windows.Forms.RadioButton rb10;
+        private System.Windows.Forms.RadioButton rb5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lSaldo;
+        private System.Windows.Forms.Label label1;
     }
 }
 
