4c2f668c354ba150425049ee27dafbb3ec222da1
Marc Meulensteen <I405582@fhict.nl>
2019-10-11 14:52:26 +0200

    Registratie from

--- Stallus/Stallus/Customer.cs
+++ Stallus/Stallus/Customer.cs
@@ -9,30 +9,36 @@ namespace Stallus
     public class Customer
     {
         string name;
+        string password;
         DateTime dateOfBirth;
-        int customerId;
+        Address address;
+        //int customerId;
         string email;
         decimal balance;
+        
 
         public string Name { get => name; private set => name = value; }
         public DateTime DateOfBirth { get => dateOfBirth; private set => dateOfBirth = value; }
-        public int CustomerId { get => customerId; private set => customerId = value; }
+        //public int CustomerId { get => customerId; private set => customerId = value; }
         public string Email { get => email; private set => email = value; }
         public decimal Balance { get => balance; private set => balance = value; }
+        public string Password { get => password; private set => password = value; }
+        public Address Address { get => address; set => address = value; }
 
-        public Customer(string name, DateTime dateOfBirth, int customerId, string email, decimal balance)
+        public Customer(string name, string password, DateTime dateOfBirth, /*int customerId,*/ string email, decimal balance, Address address)
         {
             Name = name;
+            Password = password;
+            Address = address;
             DateOfBirth = dateOfBirth;
-            CustomerId = customerId;
+            //CustomerId = customerId;
             Email = email;
             Balance = balance;
         }
 
         public override string ToString()
         {
-            return $"Customer id: {CustomerId}" +
-                   $"Name: {Name} \n" +
+            return $"Name: {Name} \n" +
                    $"Birthday: {DateOfBirth}" +
                    $"Email: {Email}" +
                    $"Balance: {Balance}";
