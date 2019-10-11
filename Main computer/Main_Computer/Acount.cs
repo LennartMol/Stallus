3a80f310d4ff7beffb055757a9e6d74ddefe406f
GlennHoeks <55874566+GlennHoeks@users.noreply.github.com>
2019-10-11 13:47:32 +0200

    Main computer aangemaakt

---
+++ Main computer/Main_Computer/Acount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Computer
+{
+    public class Acount
+    {
+        private string userName;
+
+        public string UserName { get => userName; set => userName = value; }
+        public Acount(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
