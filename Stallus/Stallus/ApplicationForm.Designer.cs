26e0623af147d5c73cad9da2200e2cd832e34782
Lennart Mol <LennartMol@users.noreply.github.com>
2019-10-25 10:35:45 +0200

    QR generator added to Stallus app

--- Stallus/Stallus/ApplicationForm.Designer.cs
+++ Stallus/Stallus/ApplicationForm.Designer.cs
@@ -31,10 +31,11 @@
             this.cbLocation = new System.Windows.Forms.ComboBox();
             this.lInCheckTime = new System.Windows.Forms.Label();
             this.lOutCheckTime = new System.Windows.Forms.Label();
-            this.btnCheckIn = new System.Windows.Forms.Button();
-            this.btnCheckOut = new System.Windows.Forms.Button();
+            this.btnLockBicycle = new System.Windows.Forms.Button();
+            this.btnUnlockBicycle = new System.Windows.Forms.Button();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.pbQRCode = new System.Windows.Forms.PictureBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.rb20 = new System.Windows.Forms.RadioButton();
             this.rb15 = new System.Windows.Forms.RadioButton();
@@ -43,79 +44,101 @@
             this.button1 = new System.Windows.Forms.Button();
             this.lSaldo = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbQRCode)).BeginInit();
             this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // cbLocation
             // 
             this.cbLocation.FormattingEnabled = true;
-            this.cbLocation.Location = new System.Drawing.Point(6, 6);
+            this.cbLocation.Location = new System.Drawing.Point(4, 5);
+            this.cbLocation.Margin = new System.Windows.Forms.Padding(2);
             this.cbLocation.Name = "cbLocation";
-            this.cbLocation.Size = new System.Drawing.Size(121, 24);
+            this.cbLocation.Size = new System.Drawing.Size(92, 21);
             this.cbLocation.TabIndex = 0;
             // 
             // lInCheckTime
             // 
             this.lInCheckTime.AutoSize = true;
-            this.lInCheckTime.Location = new System.Drawing.Point(6, 80);
+            this.lInCheckTime.Location = new System.Drawing.Point(4, 65);
+            this.lInCheckTime.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lInCheckTime.Name = "lInCheckTime";
-            this.lInCheckTime.Size = new System.Drawing.Size(46, 17);
+            this.lInCheckTime.Size = new System.Drawing.Size(35, 13);
             this.lInCheckTime.TabIndex = 1;
             this.lInCheckTime.Text = "label1";
             // 
             // lOutCheckTime
             // 
             this.lOutCheckTime.AutoSize = true;
-            this.lOutCheckTime.Location = new System.Drawing.Point(246, 80);
+            this.lOutCheckTime.Location = new System.Drawing.Point(184, 65);
+            this.lOutCheckTime.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lOutCheckTime.Name = "lOutCheckTime";
-            this.lOutCheckTime.Size = new System.Drawing.Size(46, 17);
+            this.lOutCheckTime.Size = new System.Drawing.Size(35, 13);
             this.lOutCheckTime.TabIndex = 2;
             this.lOutCheckTime.Text = "label1";
             // 
-            // btnCheckIn
+            // btnLockBicycle
             // 
-            this.btnCheckIn.Location = new System.Drawing.Point(6, 116);
-            this.btnCheckIn.Name = "btnCheckIn";
-            this.btnCheckIn.Size = new System.Drawing.Size(80, 25);
-            this.btnCheckIn.TabIndex = 3;
-            this.btnCheckIn.Text = "Check in";
-            this.btnCheckIn.UseVisualStyleBackColor = true;
+            this.btnLockBicycle.Location = new System.Drawing.Point(4, 94);
+            this.btnLockBicycle.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLockBicycle.Name = "btnLockBicycle";
+            this.btnLockBicycle.Size = new System.Drawing.Size(77, 20);
+            this.btnLockBicycle.TabIndex = 3;
+            this.btnLockBicycle.Text = "Lock bicycle";
+            this.btnLockBicycle.UseVisualStyleBackColor = true;
+            this.btnLockBicycle.Click += new System.EventHandler(this.BtnLockBicycle_Click);
             // 
-            // btnCheckOut
+            // btnUnlockBicycle
             // 
-            this.btnCheckOut.Location = new System.Drawing.Point(249, 116);
-            this.btnCheckOut.Name = "btnCheckOut";
-            this.btnCheckOut.Size = new System.Drawing.Size(80, 25);
-            this.btnCheckOut.TabIndex = 4;
-            this.btnCheckOut.Text = "Check out";
-            this.btnCheckOut.UseVisualStyleBackColor = true;
+            this.btnUnlockBicycle.Location = new System.Drawing.Point(187, 94);
+            this.btnUnlockBicycle.Margin = new System.Windows.Forms.Padding(2);
+            this.btnUnlockBicycle.Name = "btnUnlockBicycle";
+            this.btnUnlockBicycle.Size = new System.Drawing.Size(87, 20);
+            this.btnUnlockBicycle.TabIndex = 4;
+            this.btnUnlockBicycle.Text = "Unlock bicycle";
+            this.btnUnlockBicycle.UseVisualStyleBackColor = true;
+            this.btnUnlockBicycle.Click += new System.EventHandler(this.BtnUnlockBicycle_Click);
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Location = new System.Drawing.Point(9, 10);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(454, 426);
+            this.tabControl1.Size = new System.Drawing.Size(443, 432);
             this.tabControl1.TabIndex = 5;
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.pbQRCode);
             this.tabPage1.Controls.Add(this.cbLocation);
-            this.tabPage1.Controls.Add(this.btnCheckOut);
+            this.tabPage1.Controls.Add(this.btnUnlockBicycle);
             this.tabPage1.Controls.Add(this.lOutCheckTime);
-            this.tabPage1.Controls.Add(this.btnCheckIn);
+            this.tabPage1.Controls.Add(this.btnLockBicycle);
             this.tabPage1.Controls.Add(this.lInCheckTime);
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(446, 397);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Size = new System.Drawing.Size(435, 406);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Stand";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // pbQRCode
+            // 
+            this.pbQRCode.Location = new System.Drawing.Point(187, 162);
+            this.pbQRCode.Name = "pbQRCode";
+            this.pbQRCode.Size = new System.Drawing.Size(205, 205);
+            this.pbQRCode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbQRCode.TabIndex = 5;
+            this.pbQRCode.TabStop = false;
             // 
             // tabPage2
             // 
@@ -126,10 +149,11 @@
             this.tabPage2.Controls.Add(this.button1);
             this.tabPage2.Controls.Add(this.lSaldo);
             this.tabPage2.Controls.Add(this.label1);
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(446, 397);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Size = new System.Drawing.Size(435, 406);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Saldo";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -138,9 +162,10 @@
             // 
             this.rb20.AutoSize = true;
             this.rb20.Checked = true;
-            this.rb20.Location = new System.Drawing.Point(9, 193);
+            this.rb20.Location = new System.Drawing.Point(7, 157);
+            this.rb20.Margin = new System.Windows.Forms.Padding(2);
             this.rb20.Name = "rb20";
-            this.rb20.Size = new System.Drawing.Size(66, 21);
+            this.rb20.Size = new System.Drawing.Size(52, 17);
             this.rb20.TabIndex = 6;
             this.rb20.TabStop = true;
             this.rb20.Text = "€ 20,-";
@@ -149,9 +174,10 @@
             // rb15
             // 
             this.rb15.AutoSize = true;
-            this.rb15.Location = new System.Drawing.Point(9, 166);
+            this.rb15.Location = new System.Drawing.Point(7, 135);
+            this.rb15.Margin = new System.Windows.Forms.Padding(2);
             this.rb15.Name = "rb15";
-            this.rb15.Size = new System.Drawing.Size(66, 21);
+            this.rb15.Size = new System.Drawing.Size(52, 17);
             this.rb15.TabIndex = 5;
             this.rb15.Text = "€ 15,-";
             this.rb15.UseVisualStyleBackColor = true;
@@ -159,9 +185,10 @@
             // rb10
             // 
             this.rb10.AutoSize = true;
-            this.rb10.Location = new System.Drawing.Point(9, 139);
+            this.rb10.Location = new System.Drawing.Point(7, 113);
+            this.rb10.Margin = new System.Windows.Forms.Padding(2);
             this.rb10.Name = "rb10";
-            this.rb10.Size = new System.Drawing.Size(66, 21);
+            this.rb10.Size = new System.Drawing.Size(52, 17);
             this.rb10.TabIndex = 4;
             this.rb10.Text = "€ 10,-";
             this.rb10.UseVisualStyleBackColor = true;
@@ -169,9 +196,10 @@
             // rb5
             // 
             this.rb5.AutoSize = true;
-            this.rb5.Location = new System.Drawing.Point(9, 112);
+            this.rb5.Location = new System.Drawing.Point(7, 91);
+            this.rb5.Margin = new System.Windows.Forms.Padding(2);
             this.rb5.Name = "rb5";
-            this.rb5.Size = new System.Drawing.Size(58, 21);
+            this.rb5.Size = new System.Drawing.Size(46, 17);
             this.rb5.TabIndex = 3;
             this.rb5.Text = "€ 5,-";
             this.rb5.UseVisualStyleBackColor = true;
@@ -179,43 +207,56 @@
             // button1
             // 
             this.button1.AutoSize = true;
-            this.button1.Location = new System.Drawing.Point(9, 220);
+            this.button1.Location = new System.Drawing.Point(7, 179);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(108, 27);
+            this.button1.Size = new System.Drawing.Size(85, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Raise balance";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // lSaldo
             // 
             this.lSaldo.AutoSize = true;
-            this.lSaldo.Location = new System.Drawing.Point(58, 45);
+            this.lSaldo.Location = new System.Drawing.Point(44, 37);
+            this.lSaldo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lSaldo.Name = "lSaldo";
-            this.lSaldo.Size = new System.Drawing.Size(16, 17);
+            this.lSaldo.Size = new System.Drawing.Size(13, 13);
             this.lSaldo.TabIndex = 1;
             this.lSaldo.Text = "0";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 45);
+            this.label1.Location = new System.Drawing.Point(4, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.Size = new System.Drawing.Size(37, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Saldo:";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(187, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(203, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Scan this QR code to unlock your bicycle";
+            // 
             // ApplicationForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(712, 521);
             this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "ApplicationForm";
             this.Text = "Form1";
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbQRCode)).EndInit();
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
@@ -227,8 +268,8 @@
         private System.Windows.Forms.ComboBox cbLocation;
         private System.Windows.Forms.Label lInCheckTime;
         private System.Windows.Forms.Label lOutCheckTime;
-        private System.Windows.Forms.Button btnCheckIn;
-        private System.Windows.Forms.Button btnCheckOut;
+        private System.Windows.Forms.Button btnLockBicycle;
+        private System.Windows.Forms.Button btnUnlockBicycle;
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
@@ -239,6 +280,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label lSaldo;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pbQRCode;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Stallus/Stallus/ApplicationForm.cs
+++ Stallus/Stallus/ApplicationForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using QRCoder;
 
 namespace Stallus
 {
@@ -17,9 +18,23 @@ namespace Stallus
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void BtnLockBicycle_Click(object sender, EventArgs e)
         {
+            // A message has to be send to the main computer to lock the bicycle.
+        }
 
+        private void BtnUnlockBicycle_Click(object sender, EventArgs e)
+        {
+            // A QR code is generated and the message within the QR code is send to the main computer to verificate the user's location (is he/she at the bicycle stand?)
+
+            string generatedString = "Test";
+
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(generatedString, QRCodeGenerator.ECCLevel.H);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            pbQRCode.Image = qrCodeImage;
         }
     }
 }
