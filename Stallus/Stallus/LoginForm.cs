d1862f9fe129855e4985d98f519cf47b7ab8ac45
Marc Meulensteen <I405582@fhict.nl>
2019-10-25 10:16:46 +0200

    onzin

--- Stallus/Stallus/LoginForm.cs
+++ Stallus/Stallus/LoginForm.cs
@@ -19,35 +19,10 @@ namespace Stallus
 
         private void BtOpenApplication_Click(object sender, EventArgs e)
         {
-            Form popupp = new Form();
-            popupp.Text = "Qr code";
-            PictureBox pb = new PictureBox();
-            pb.Image = Image.FromFile("C:/Users/mpcme/OneDrive/Afbeeldingen/Shrek.JPG");
-            pb.Location = new Point(25, 25);
-            Size pictureBoxSize = new Size(300, 300);
-            pb.Size = pictureBoxSize;
-            Size formSize = new Size(365, 385);
-            popupp.Size = formSize;
-            pb.SizeMode = PictureBoxSizeMode.StretchImage;
-            
-            popupp.Controls.Add(pb);
-            popupp.ShowDialog();
 
-            if (/*Qr scan is success */true)
-            {
-                popupp.Close();
                 ApplicationForm app = new ApplicationForm();
                 this.Hide();
                 app.ShowDialog();
-            }
-
-
-
-
-
-
-
-
 
         }
 
