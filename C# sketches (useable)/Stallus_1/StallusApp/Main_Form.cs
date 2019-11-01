3a1c8269274d6f3107463f652f132bba4f51cb4b
SanderWesseldijk <55689512+SanderWesseldijk@users.noreply.github.com>
2019-11-01 13:14:19 +0100

    Changed server a little

---
+++ C# sketches (useable)/Stallus_1/StallusApp/Main_Form.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StallusApp
+{
+    public partial class Main_Form : Form
+    {
+        QR_Code qr = new QR_Code();
+        public Main_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateQRCode_Button_Click(object sender, EventArgs e)
+        {
+            GraphicQRCode_PB.Image = qr.GenerateQR_Code("#USER_ID:001%");
+        }
+    }
+}
---
+++ C# sketches (useable)/Stallus_1/StallusApp/QR_Code.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using QRCoder;
+
+namespace StallusApp
+{
+    class QR_Code
+    {
+        public Image GenerateQR_Code(string text)
+        {
+            QRCodeGenerator generator = new QRCodeGenerator();
+            //string nowDate = DateTime.Now.ToString("dd/MM/yy");
+            //string nowTime = DateTime.Now.ToString("hh:mm:ss");
+            QRCodeData data = generator.CreateQrCode("#" + text + /*";MOMENT:" + nowDate + "@" + nowTime +*/ "%", QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(data);
+            return qrCode.GetGraphic(5);
+        }
+    }
+}
